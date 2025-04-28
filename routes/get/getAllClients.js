3f2202a146068af6119263c74842ad59d6c64000
joaoneto9 <joaonetocg1@gmail.com>
2025-04-28 15:02:53 -0300

    feat: requisicao GET para pegar todos d=os clientes foi implementada com sucesso

---
+++ routes/get/getAllClients.js
@@ -0,0 +1,16 @@
+import { Client } from "../../client.js";
+
+async function getAllClients() {
+    return fetch('http://localhost:8080/clients')  // Faz a requisição
+        .then(async response => {
+            const data = await response.json();  // Aguarda a conversão da resposta para JSON
+            const clients = data.map(c => new Client(c.name, c.email, c.orders));  // Mapeia para a classe Client
+            return clients;  // Retorna o array de clientes
+        })
+        .catch(error => {
+            console.log('Erro ao buscar clientes:', error);  // Trata qualquer erro
+            return [];  // Retorna um array vazio em caso de erro
+        });
+}
+
+export const allClients = await getAllClients();  // Agora 'await' está corretamente dentro de uma função async
